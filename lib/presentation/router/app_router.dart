071e7070f2a5dd9f1070f988e24085f89be2e35d
devdbsdngml <wh9614y@gmail.com>
2026-02-03 00:47:06 +0900

    feat: add routes of each screen

--- lib/presentation/router/app_router.dart
+++ lib/presentation/router/app_router.dart
@@ -1,6 +1,9 @@
 import 'package:go_router/go_router.dart';
 import 'package:flutter/material.dart';
+import 'package:gongbab_owner/presentation/screens/company_meal_detail/company_meal_detail_screen.dart';
+import 'package:gongbab_owner/presentation/screens/daily_meal_count_status/daily_meal_count_status_screen.dart';
 import 'package:gongbab_owner/presentation/screens/login/login_screen.dart';
+import 'package:gongbab_owner/presentation/screens/monthly_settlement/monthly_settlement_screen.dart';
 
 part 'app_routes.dart';
 
@@ -12,6 +15,27 @@ class AppRouter {
         path: AppRoutes.login,
         builder: (BuildContext context, GoRouterState state) {
           return const LoginScreen();
+        },
+      ),
+      GoRoute(
+        path: AppRoutes.dailyMealCountStatus,
+        builder: (BuildContext context, GoRouterState state) {
+          return const DailyMealCountStatusScreen();
+        },
+      ),
+      GoRoute(
+        path: AppRoutes.companyMealDetail,
+        builder: (BuildContext context, GoRouterState state) {
+          return CompanyMealDetailScreen(
+            companyName: state.pathParameters['companyName'] ?? '',
+            selectedDate: state.pathParameters['email'] as DateTime? ?? DateTime.now(),
+          );
+        },
+      ),
+      GoRoute(
+        path: AppRoutes.monthlySettlement,
+        builder: (BuildContext context, GoRouterState state) {
+          return const MonthlySettlementScreen();
         },
       ),
       // Add other routes here
--- lib/presentation/router/app_routes.dart
+++ lib/presentation/router/app_routes.dart
@@ -2,5 +2,8 @@ part of 'app_router.dart';
 
 abstract class AppRoutes {
   static const String login = '/login';
+  static const String dailyMealCountStatus = '/daily_meal_count_status';
+  static const String companyMealDetail = '/company_meal_detail';
+  static const String monthlySettlement = '/monthly_settlement';
   // Add other routes here
 }
